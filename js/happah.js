5df3891dbbbba4a92255317f0f69cf3b62f7e0ec
Tarek <tarek@argos.ivd.kit.edu>
2015-12-04 14:39:19 +0100

    happah.js: The 'drawPointCloud' now automatically connects two points with a line. They are connected in the order of the point array.

--- js/happah.js
+++ js/happah.js
@@ -1,10 +1,3 @@
-// TODO Remove global variables!
-var _cam;
-var _scene;
-var _renderer;
-var _controls;
-
-
 var happah = {
   scene: function(/*options*/) {
     // Create new scene.
@@ -17,16 +10,18 @@ var happah = {
     return scene;
   },
 
+  /**
+   * Initializes with standard settings,
+   * such as: 1 camera, 1 scene, 1 renderer.
+   */
   init: function() {
     var scene = this.scene();
     var camera = DEFAULTS.getCamera();
     var renderer = DEFAULTS.getRenderer();
     var controls = DEFAULTS.getControls(camera);
+
+    // Grid for testing purposes.
     var grid = DEFAULTS.getGrid();
-    _scene = scene;
-    _cam = camera;
-    _renderer = renderer;
-    _controls = controls;
     scene.add(grid);
 
     // Standard settings
@@ -40,6 +35,9 @@ var happah = {
     console.log("happah initialized.");
   },
 
+  /**
+   * Draws an array of points.
+   */
   drawPointCloud: function(points) {
     //TODO: points should be circles not squares
     var pointGeometry = new THREE.Geometry();
@@ -47,21 +45,31 @@ var happah = {
     var pointCloud = new THREE.PointCloud(pointGeometry, material);
 
     for (i = 0; i < points.length; i++) {
-      console.log("pushing point at: " + points[i][0] + points[i][1] + points[i][2]);
-      pointGeometry.vertices.push(new THREE.Vector3(points[i][0], points[i][1], points[i][2]));
+      pointGeometry.vertices.push(new THREE.Vector3(points[i][0],
+                                                    points[i][1],
+                                                    points[i][2]));
     }
-    happah._scene.add(pointCloud);
+    // Connect the points with lines.
+    var line = new THREE.Line(pointGeometry,
+               new THREE.LineBasicMaterial({color:0xccafff, opacity: 0.5 }));
+
+    // Add to scene:
+    happah.scene.add(line);
+    happah.scene.add(pointCloud);
 
   },
 
+  drawLines: function() {
+    //this.pointGeometry.vertices.push(1,1,1);
+  },
   objects: function() {
     //TODO: get objects from memory somehow
   },
 
-  render: function(objects) {
+  render: function(c, s, x) {
     requestAnimationFrame( happah.render );
-    happah._controls.update();
-    happah._renderer.render(happah._scene, happah._cam);
+
+    happah.renderer.render(s, x);
   },
 
   addObjectToScene: function(object) {
@@ -75,7 +83,7 @@ var happah = {
  */
 var DEFAULTS = {
   getCamera: function(/* options */) {
-  return new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    return new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   },
 
   getRenderer: function(/* options */) {
@@ -87,7 +95,7 @@ var DEFAULTS = {
   },
 
   getControls: function(camera) {
-   return new THREE.TrackballControls(camera);
+    return new THREE.TrackballControls(camera);
   },
 
   getLight: function(color) {
@@ -96,18 +104,18 @@ var DEFAULTS = {
 };
 
 /* // Different approach
-var DEFAULTS = [
-  // Default perspective camera.
-  "new THREE.PerspectiveCamera(75, window.innerWidth /window.innerHeight, 0.1, 1000)",
+   var DEFAULTS = [
+// Default perspective camera.
+"new THREE.PerspectiveCamera(75, window.innerWidth /window.innerHeight, 0.1, 1000)",
 
-  // Default renderer.
-  "new THREE.WebGLRenderer()",
+// Default renderer.
+"new THREE.WebGLRenderer()",
 
-  // Default controls
-  "new THREE.TrackballControls(camera)",
+// Default controls
+"new THREE.TrackballControls(camera)",
 
-  // Default grid.
-  "new THREE.GridHelper(100, 10)"
+// Default grid.
+"new THREE.GridHelper(100, 10)"
 
 ];
 */
