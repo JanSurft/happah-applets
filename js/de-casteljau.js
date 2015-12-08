16137f14ead2f613df79b8e3747d5628f820ab95
Stephan Engelmann <stephan.engelmann@student.kit.edu>
2015-12-08 13:50:39 +0100

    File reformating.
    * Added remote locations for three.min.js
    * default tabwidth in project is 5 spaces

--- js/de-casteljau.js
+++ js/de-casteljau.js
@@ -1,15 +1,13 @@
-//
-//
 var init = function() {
-  var scene = happah.scene();
-  var camera = DEFAULTS.getCamera();
-  var renderer = DEFAULTS.getRenderer();
-  var controls = DEFAULTS.getControls(camera);
-  var grid = DEFAULTS.getGrid();
-  scene.add(grid);
+     var scene = happah.scene();
+     var camera = DEFAULTS.getCamera();
+     var renderer = DEFAULTS.getRenderer();
+     var controls = DEFAULTS.getControls(camera);
+     var grid = DEFAULTS.getGrid();
+     scene.add(grid);
 
-  camera.position.z=5;
-  controls.target.set(0,0,0);
-  renderer.setSize(300,300);
-  document.body.appendChild(renderer.domElement);
+     camera.position.z=5;
+     controls.target.set(0,0,0);
+     renderer.setSize(300,300);
+     document.body.appendChild(renderer.domElement);
 }
--- js/happah.js
+++ js/happah.js
@@ -1,111 +1,111 @@
 var controls;
 
 var happah = {
-  scene: function(/*options*/) {
-    // Create new scene.
-    var scene = new THREE.Scene();
+     scene: function(/*options*/) {
+          // Create new scene.
+          var scene = new THREE.Scene();
 
-    // Add optional values
-    // scene.add();
+          // Add optional values
+          // scene.add();
 
-    // Return the final scene.
-    return scene;
-  },
+          // Return the final scene.
+          return scene;
+     },
 
-  /**
-   * Initializes with standard settings,
-   * such as: 1 camera, 1 scene, 1 renderer.
-   */
-  init: function() {
-    var scene = this.scene();
-    var camera = DEFAULTS.getCamera();
-    var renderer = DEFAULTS.getRenderer();
-    var controls = DEFAULTS.getControls(camera);
+     /**
+      * Initializes with standard settings,
+      * such as: 1 camera, 1 scene, 1 renderer.
+      */
+     init: function() {
+          var scene = this.scene();
+          var camera = DEFAULTS.getCamera();
+          var renderer = DEFAULTS.getRenderer();
+          var controls = DEFAULTS.getControls(camera);
 
-    // Grid for testing purposes.
-    var grid = DEFAULTS.getGrid();
-    scene.add(grid);
+          // Grid for testing purposes.
+          var grid = DEFAULTS.getGrid();
+          scene.add(grid);
 
-    // Standard settings
-    camera.position.z = 20;
-    controls.target.set( 0,0,0);
+          // Standard settings
+          camera.position.z = 20;
+          controls.target.set( 0,0,0);
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
+          renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // Append to document.
-    document.body.appendChild(renderer.domElement);
-    console.log("happah initialized.");
-  },
+          // Append to document.
+          document.body.appendChild(renderer.domElement);
+          console.log("happah initialized.");
+     },
 
 
 
-  /**
-   * Draws an array of points.
-   */
-  drawPointCloud: function(points) {
-    //TODO: points should be circles not squares
-    var pointGeometry = new THREE.Geometry();
-    var material = new THREE.PointCloudMaterial( { size: 3, sizeAttenuation: false });
-    var pointCloud = new THREE.PointCloud(pointGeometry, material);
+     /**
+      * Draws an array of points.
+      */
+     drawPointCloud: function(points) {
+          //TODO: points should be circles not squares
+          var pointGeometry = new THREE.Geometry();
+          var material = new THREE.PointCloudMaterial( { size: 3, sizeAttenuation: false });
+          var pointCloud = new THREE.PointCloud(pointGeometry, material);
 
-    // Adds all the points to pointgeometry.
-    for (i = 0; i < points.length; i++) {
-      pointGeometry.vertices.push(new THREE.Vector3(points[i][0],
-            points[i][1],
-            points[i][2]));
-    }
-    // Connect the points with lines.
-    var line = new THREE.Line(pointGeometry,
-        new THREE.LineBasicMaterial({color:0xccafff, opacity: 0.5 }));
+          // Adds all the points to pointgeometry.
+          for (i = 0; i < points.length; i++) {
+               pointGeometry.vertices.push(new THREE.Vector3(points[i][0],
+                              points[i][1],
+                              points[i][2]));
+          }
+          // Connect the points with lines.
+          var line = new THREE.Line(pointGeometry,
+                    new THREE.LineBasicMaterial({color:0xccafff, opacity: 0.5 }));
 
-    // Add to scene:
-    happah.scene.add(line);
-    happah.scene.add(pointCloud);
+          // Add to scene:
+          happah.scene.add(line);
+          happah.scene.add(pointCloud);
 
-  },
+     },
 
-  objects: function() {
-    //TODO: get objects from memory somehow
-  },
+     objects: function() {
+          //TODO: get objects from memory somehow
+     },
 
-  // Renders the scene in each renderer.
-  render: function(renderer, controls) {
-    requestAnimationFrame(happah.render);
+     // Renders the scene in each renderer.
+     render: function(renderer, controls) {
+          requestAnimationFrame(happah.render);
 
-    // Update the controls. FIXME: works only @ first call.
-    // probably something with 'requestAnimationFrame'
-    // (render does not take arguments there..)
-    this.controls.update();
+          // Update the controls. FIXME: works only @ first call.
+          // probably something with 'requestAnimationFrame'
+          // (render does not take arguments there..)
+          this.controls.update();
 
-    for (i = 0; i < renderer.length; i++) {
-      renderer[i].render(happah.scene, happah.cam);
-    }
-  }
+          for (i = 0; i < renderer.length; i++) {
+               renderer[i].render(happah.scene, happah.cam);
+          }
+     }
 }
 
 /**
  * Holds default values for simple viewport setup.
  */
 var DEFAULTS = {
-  getCamera: function(/* options */) {
-    return new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  },
+     getCamera: function(/* options */) {
+          return new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+     },
 
-  getRenderer: function(/* options */) {
-    return new THREE.WebGLRenderer();
-  },
+     getRenderer: function(/* options */) {
+          return new THREE.WebGLRenderer();
+     },
 
-  getGrid: function() {
-    return new THREE.GridHelper(100, 10);
-  },
+     getGrid: function() {
+          return new THREE.GridHelper(100, 10);
+     },
 
-  getControls: function(camera) {
-    return new THREE.TrackballControls(camera);
-  },
+     getControls: function(camera) {
+          return new THREE.TrackballControls(camera);
+     },
 
-  getLight: function(color) {
-    return new THREE.AmbientLight(color);
-  }
+     getLight: function(color) {
+          return new THREE.AmbientLight(color);
+     }
 };
 
 /* // Different approach
