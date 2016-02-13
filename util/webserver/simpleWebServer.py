9b5316a2a5580ee2c0811a0305a15b4ad3655e85
Stephan Engelmann <stephan.engelmann@student.kit.edu>
2016-02-13 11:01:05 +0100

    Add minimal webserver implementation

---
+++ util/webserver/simpleWebServer.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+import http.server
+import socketserver
+
+class MySimpleHTTPHandler( http.server.SimpleHTTPRequestHandler ):
+     def send_head(self):
+          self.send_header("Access-Control-Allow-Origin", "*")
+          super.send_head(self)
+
+PORT = 80
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
