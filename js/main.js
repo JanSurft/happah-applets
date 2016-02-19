30345dfd67d865b4f8ff3a3cf010c33b55b8a631
Pawel Herman <pawel.herman@kit.edu>
2016-02-19 12:10:30 +0100

    added scene and viewport

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ require.config({
      shim: {
           'three': { exports: 'THREE' },
           'TrackballControls': { deps: ['three'], exports: 'THREE' },
-          'Projector': { deps: ['three'], exports: 'THREE' },
           'TransformControls': { deps: ['three'], exports: 'THREE' },
           'DragControls': { deps: ['three'], exports: 'THREE' }
      },
@@ -11,7 +10,6 @@ require.config({
           jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min",
           three: "http://threejs.org/build/three",
           TrackballControls: "http://threejs.org/examples/js/controls/TrackballControls",
-          Projector: "http://threejs.org/examples/js/renderers/Projector",
           TransformControls: "http://threejs.org/examples/js/controls/TransformControls",
           DragControls: "http://threejs.org/examples/js/controls/DragControls"
           //shader: '../lib/shader',
@@ -19,10 +17,13 @@ require.config({
      }
 });
 
-require([ 'happah' ], function (happah) {
-     var gui = new happah.GUI;
-     gui.init();
-     gui.setAlgorithm(HAPPAH.algorithms.deCasteljau);
-     gui.animate();
+require([ 'happah', 'three' ], function (happah, THREE) {
+     var scene = new happah.Scene();
+     scene.algorithm = HAPPAH.algorithms.deCasteljau;
+     for (i = 0; i < 3; i++) scene.addControlPoint(new THREE.Vector3(i / 2, i * 3, Math.sin(i)));//TODO: get rid of THREE
+     var viewport = new happah.Viewport(scene);
+     viewport.update();
+     viewport.animate();
+     console.log("happah initialized.");
 });
 
