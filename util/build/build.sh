547e1d6d45fc78eacce15b72ba7bfea5d80a7295
Stephan Engelmann <stephan.engelmann@student.kit.edu>
2016-03-09 14:45:39 +0100

    fix build output js dir

--- util/build/build.sh
+++ util/build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-python2 build.py --include common --output ../../build/shaders.js
-python2 build.py --include common --minify --output ../../build/shaders.min.js
+python2 build.py --include common --output ../../build/js/shaders.js
+python2 build.py --include common --minify --output ../../build/js/shaders.min.js
