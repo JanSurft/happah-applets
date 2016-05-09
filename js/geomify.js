510c5196bba916c2f66215f8ef4093e2fa79d5b2
Tarek Wilkening <tarek_wilkening@web.de>
2016-05-09 23:11:16 +0200

    change scale at axis arrow
    add new file geomify.js that takes a symbol and returns
    a geometry in shape of the symbol

---
+++ js/geomify.js
@@ -0,0 +1,68 @@
+function geomify(string) {
+    // Create new geometry
+    var textGeo = new THREE.Geometry();
+
+    switch (string) {
+        case '1':
+            //            _
+            //           /' \
+            //          /\_, \
+            //          \/_/\ \
+            //             \ \ \
+            //              \ \_\
+            //               \/_/
+            textGeo.vertices.push(new THREE.Vector3(1, 0, 0));
+            textGeo.vertices.push(new THREE.Vector3(2, 0, 0));
+            textGeo.vertices.push(new THREE.Vector3(2, 4, 0));
+            textGeo.vertices.push(new THREE.Vector3(0, 4, 0));
+            textGeo.vertices.push(new THREE.Vector3(0, 3, 0));
+            textGeo.vertices.push(new THREE.Vector3(1, 3, 0));
+            textGeo.faces.push(new THREE.Face3(0, 1, 2));
+            textGeo.faces.push(new THREE.Face3(0, 2, 5));
+            textGeo.faces.push(new THREE.Face3(2, 3, 5));
+            textGeo.faces.push(new THREE.Face3(3, 4, 5));
+            break;
+        case '0':
+            //            __
+            //          /'__`\
+            //         /\ \/\ \
+            //         \ \ \ \ \
+            //          \ \ \_\ \
+            //           \ \____/
+            //            \/___/
+            textGeo.vertices.push(new THREE.Vector3(1, 0, 0));
+            textGeo.vertices.push(new THREE.Vector3(2, 0, 0));
+            textGeo.vertices.push(new THREE.Vector3(3, 1, 0));
+            textGeo.vertices.push(new THREE.Vector3(3, 3, 0));
+            textGeo.vertices.push(new THREE.Vector3(2, 4, 0));
+            textGeo.vertices.push(new THREE.Vector3(1, 4, 0));
+            textGeo.vertices.push(new THREE.Vector3(0, 3, 0));
+            textGeo.vertices.push(new THREE.Vector3(0, 1, 0));
+
+            textGeo.vertices.push(new THREE.Vector3(1, 1, 0));
+            textGeo.vertices.push(new THREE.Vector3(2, 1, 0));
+            textGeo.vertices.push(new THREE.Vector3(2, 3, 0));
+            textGeo.vertices.push(new THREE.Vector3(1, 3, 0));
+
+            textGeo.faces.push(new THREE.Face3(0, 1, 7));
+            textGeo.faces.push(new THREE.Face3(7, 1, 2));
+            textGeo.faces.push(new THREE.Face3(2, 3, 9));
+            textGeo.faces.push(new THREE.Face3(9, 3, 10));
+            textGeo.faces.push(new THREE.Face3(3, 4, 5));
+            textGeo.faces.push(new THREE.Face3(3, 5, 6));
+            textGeo.faces.push(new THREE.Face3(6, 7, 8));
+            textGeo.faces.push(new THREE.Face3(6, 8, 11));
+            break;
+        case '-1':
+            //             _
+            //            /' \
+            //           /\_, \
+            //   _______ \/_/\ \
+            //  /\______\   \ \ \
+            //          /    \ \_\
+            //                \/_/
+            break;
+    }
+
+    return textGeo;
+}
