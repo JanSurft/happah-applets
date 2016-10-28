273c65641ea5ef28e48c3a006240edf0080c7982
Stephan Engelmann <stephan-engelmann@gmx.de>
2016-10-28 15:42:02 +0200

    adapt build script to new folder structure

--- util/build/build.sh
+++ util/build/build.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-mkdir -p ../../build/js
-mkdir -p ../../build/i18n
+mkdir -p ../../js/build
 
 # join custom shaders in one file
-python2 build.py --include common --output ../../build/js/shaders.js
-python2 build.py --include common --minify --output ../../build/js/shaders.min.js
+python2 build.py --include common --output ../../js/build/shaders.js
+python2 build.py --include common --minify --output ../../js/build/shaders.min.js
 
 # generate lang files
-for dir in ../../applets/*/
+for dir in ../../html/*/
 do
-  app_name=$(basename "$dir")
-  mkdir -p ../../applets/$app_name/build/i18n
-  echo " * generating locales for app $app_name"
+  app_dir=$(basename "$dir")
+  mkdir -p $dir/build/i18n
+  echo " * generating i18n locales for app $app_dir"
   for file in $dir/i18n/*.yaml
     do
         filename=$(basename "$file")
         filename="${filename%.*}"
-        out_file=../../applets/$app_name/build/i18n/$filename.json
+        out_file=${dir}build/i18n/$filename.json
         python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < $file > $out_file
     done
 done
