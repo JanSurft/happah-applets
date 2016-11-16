fb23a9619035d52d5d5ec20e1842bcb9b5da7466
Tarek Wilkening <tarek_wilkening@web.de>
2016-11-16 13:50:40 +0100

    add reference to overlay camera to labelmanager
    in order to project the labels position correctly to the screen,
    a reference to the overlay camera was necessary.
    
     * add overview flag to addLabel method which decides whether the label
       belongs to the scene or the overlay (default false)

--- js/lib/labelmanager.js
+++ js/lib/labelmanager.js
@@ -4,22 +4,21 @@
  //
  //////////////////////////////////////////////////////////////////////////////
  define(['jquery', 'three'], function($, THREE) {
-      const label_offset_y = 10;
+      //const label_offset_y = 10;
       const label_color = "black";
 
       var s_labelCount = Symbol('labelcount');
-      var s_camera = Symbol('camera');
+      var s_sceneCamera = Symbol('scenecamera');
+      var s_overlayCamera = Symbol('overlaycamera');
       var s_labels = Symbol('labels');
-      var s_topOffset = Symbol('topoffset');
       var s_positions = Symbol('positions');
 
       class LabelManager {
 
-           constructor(camera) {
-                this.update = this.update.bind(this);
-
+           constructor(sceneCamera, overviewCamera) {
                 this[s_labelCount] = 0;
-                this[s_camera] = camera;
+                this[s_sceneCamera] = sceneCamera;
+                this[s_overlayCamera] = overviewCamera;
                 this[s_labels] = [];
                 this[s_positions] = [];
            }
@@ -31,29 +30,7 @@
                 element.removeEventListener('update', this.update, false);
            }
 
-           update() {
-                var canvas = $(".hph-canvas")[0];
-                // The scene has changed so we need to update our
-                // labels' positions
-                for (var i in this[s_labels]) {
-                     var projected = this[s_positions][i].clone();
-                     projected.project(this[s_camera]);
-                     projected.x = Math.round((projected.x + 1) * canvas.width / 2);
-                     projected.y = Math.round((-projected.y + 1) * canvas.height / 2) + label_offset_y;
-
-                     // Limit to canvas frame
-                     projected.x = (projected.x > canvas.width - 20) ? canvas.width - 20 : projected.x;
-                     projected.y = (projected.y > canvas.height - 20) ? canvas.height - 20 : projected.y;
-
-                     projected.max(new THREE.Vector3(0, 0, 0));
-
-                     this[s_labels][i].css("left", this[s_positions][i].x + "px");
-                     this[s_labels][i].css("top", this[s_positions][i].y + "px");
-                }
-                console.log("labelmanager.update");
-           }
-
-           addLabel(text, position, tag = "") {
+           addLabel(text, position, tag = "", overview = false) {
                 // Create a new container
                 $("#hph-canvas-wrapper").append("<div class=" + "label" + tag + this[s_labelCount] + "></div>");
 
@@ -71,9 +48,14 @@
 
                 // Transform world-position vector to pixel coordinates
                 var pos = position.clone();
-                pos.project(this[s_camera]);
+
+                if (overview) {
+                     pos.project(this[s_overlayCamera]);
+                } else {
+                     pos.project(this[s_sceneCamera]);
+                }
                 pos.x = Math.round((pos.x + 1) * canvas.width / 2);
-                pos.y = Math.round((-pos.y + 1) * canvas.height / 2) + label_offset_y;
+                pos.y = Math.round((-pos.y + 1) * canvas.height / 2);
 
                 // Limit to canvas frame
                 pos.x = (pos.x > canvas.width - 20) ? canvas.width - 20 : pos.x;
