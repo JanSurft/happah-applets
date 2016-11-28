eba3d2938b7e5d19cfc45de94bdb40da0a866d73
Stephan Engelmann <stephan-engelmann@gmx.de>
2016-11-28 18:53:18 +0100

    add missing lines to linear precision applet

--- js/precision/algorithm.js
+++ js/precision/algorithm.js
@@ -24,6 +24,8 @@ define(['jquery', 'three', 'lib/happah', 'lib/util'], function($, THREE, HAPPAH,
      var s_controlPoints = Symbol('controlPoints');
      var s_ratio = Symbol('ratio');
      var s_scrollbar = Symbol('scrollbar');
+     const EMPH_COLOR = 0xFF0000;
+     const LINE_COLOR = 0x888888;
 
      class Algorithm extends HAPPAH.DeCasteljauAlgorithm {
 
@@ -53,63 +55,63 @@ define(['jquery', 'three', 'lib/happah', 'lib/util'], function($, THREE, HAPPAH,
                } else {
                     ratio = this[s_scrollbar].value;
                }
-               // Create the first frame by hand
+
                var storyboard = new HAPPAH.Storyboard(this);
-               var frame0 = new HAPPAH.Storyboard.Frame();
-               frame0.lines[0] = UTIL.Util.insertSegmentStrip(this[s_controlPoints], 0xff0000);
-               frame0.title = "Controlpolygon";
-               storyboard.append(frame0);
 
-               if (this[s_controlPoints].length == 0) {
-                    // Add a dummy mesh
-                    frame0.lines[0] = new THREE.Object3D();
-                    return storyboard;
-               }
-
-               // each iteration step gets stored in a matrix row
+               // evaluate all de Casteljau steps and store their result in a
+               // point matrix
                var pointMatrix = new Array();
-
-               // First set of points is the control polygon
                pointMatrix.push(this[s_controlPoints]);
-
-               // fill matrix with points from each iteration
                this.evaluate(ratio, function add(points) {
                     pointMatrix.push(points);
                });
 
-               var redSegments = [];
-               var blackSegments = [];
-               var impostorPoints = [];
-
-               for (var frameIndex = 1; frameIndex < pointMatrix.length; frameIndex++) {
+               for (var currentFrame = 0; currentFrame < pointMatrix.length; currentFrame++) {
                     var frame = new HAPPAH.Storyboard.Frame();
-                    var offset = 20;
-                    for (var row = 1; row <= frameIndex; row++) {
+                    var offset = 0;
+                    for (var row = 0; row <= currentFrame; row++) {
                          for (var i in pointMatrix[row]) {
                               var point = pointMatrix[row][i].clone();
                               point.y += offset;
                               frame.points.push(point);
                          }
-                         if (row == frameIndex && frameIndex != pointMatrix.length - 1) {
-                              //for (var i = 0; i < pointMatrix[row].length - 1; i++) {
+                         // draw each newly generated line with an emphasized
+                         // color, for example red
+                         if (row == currentFrame && currentFrame != pointMatrix.length - 1) {
                               var start = pointMatrix[row][0].clone();
                               start.y += offset;
                               var end = pointMatrix[row][pointMatrix[row].length - 1].clone();
                               end.y += offset;
-                              var segment = new Array();
-                              segment.push(start);
-                              segment.push(end);
                               var segment = [start, end];
-                              var strip = UTIL.Util.insertSegmentStrip(segment, 0xFF0000);
+                              var strip = UTIL.Util.insertSegmentStrip(segment, EMPH_COLOR);
                               frame.lines.push(strip);
-                              //}
+                         // draw lines from previous steps with another color
+                         } else if (row < currentFrame) {
+                              var start = pointMatrix[row][0].clone();
+                              start.y += offset;
+                              var end = pointMatrix[row][pointMatrix[row].length - 1].clone();
+                              end.y += offset;
+                              var segment = [start, end];
+                              var strip = UTIL.Util.insertSegmentStrip(segment, LINE_COLOR);
+                              frame.lines.push(strip);
                          }
-                         offset += 20;
+                         offset += 10;
                     }
-
+                    offset -= 10;
                     storyboard.append(frame);
-               }
 
+                    // add an additional frame where the new points are on the
+                    // previous lines
+                    if (currentFrame != pointMatrix.length - 1) {
+                         var midFrame = frame.clone();
+                         for (var i in pointMatrix[currentFrame + 1]) {
+                              var pointCopy = pointMatrix[currentFrame + 1][i].clone();
+                              pointCopy.y += offset;
+                              midFrame.points.push(pointCopy);
+                         }
+                         storyboard.append(midFrame);
+                    }
+               }
                return storyboard;
           }
      }
