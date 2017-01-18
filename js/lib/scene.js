86be51bed5761e20f2aa96fac4c0be0c43b28373
Tarek Wilkening <tarek_wilkening@web.de>
2017-01-18 14:49:31 +0100

    remove obsolete scene.redraw method

--- js/lib/scene.js
+++ js/lib/scene.js
@@ -8,8 +8,6 @@
 define(['jquery', 'three', './spherical-impostor'], function($, THREE, happah) {
      /** Flags for drawing preferences */
 
-     // Set to true if scene has been altered.
-     var s_altered = Symbol('altered');
      var s_showCurve = Symbol('showcurve');
 
      // If set: will draw control polygon
@@ -30,38 +28,12 @@ define(['jquery', 'three', './spherical-impostor'], function($, THREE, happah) {
                     this[s_points] = new THREE.Object3D();
                     this[s_lines] = [new THREE.Object3D()];
                     this._controlPointImpostors = new THREE.Object3D();
-                    this[s_altered] = true;
                     this[s_showPoly] = true;
                     this[s_showCurve] = true;
                }
 
                get controlPointImpostors() {
                     return this._controlPointImpostors;
-               }
-
-               set controlPolygonState(state) {
-                    this[s_showPoly] = state;
-                    //this.redraw();
-                    $.event.trigger({
-                         type: "change",
-                         // For easy debugging
-                         message: "control-polygon was toggled!",
-                    });
-                    if (this[s_showPoly]) {
-                         this.add(this._controlPointImpostors);
-                    } else {
-                         // Don't draw impostors
-                         this.remove(this._controlPointImpostors);
-                    }
-               }
-
-               set curveState(state) {
-                    this[s_showCurve] = state;
-                    //this.redraw();
-                    $.event.trigger({
-                         type: "change",
-                         message: "curve state has changed!",
-                    });
                }
 
                set lines(lines) {
@@ -71,7 +43,6 @@ define(['jquery', 'three', './spherical-impostor'], function($, THREE, happah) {
                          this[s_lines].add(lines[i])
                     }
                     this.add(this[s_lines])
-                         //this.redraw();
                          //$.event.trigger({
                          //type: "change",
                          //message: "lines have been set!",
@@ -82,25 +53,13 @@ define(['jquery', 'three', './spherical-impostor'], function($, THREE, happah) {
                     this.remove(this[s_points]);
                     this[s_points] = points;
                     this.add(points);
-                    //this.redraw();
                     //$.event.trigger({
                     //type: "change",
                     //message: "points have been set!",
                     //});
                }
 
-               // remove this later
-               get altered() {
-                    return this[s_altered];
-               }
-
-               /** Redraws the curve in the next animate() cycle */
-               redraw() {
-                    this[s_altered] = true;
-               }
-
                paint() {
-                    this[s_altered] = false;
 
                     // The very first mesh is the control-polygon
                     if (this[s_showPoly]) {
@@ -121,7 +80,7 @@ define(['jquery', 'three', './spherical-impostor'], function($, THREE, happah) {
                     //TODO: why set length to 0?
                     this.controlPoints.length = 0;
                     this._controlPointImpostors.children = [];
-                    // FIRE CUSTOM JQUERY EVENT
+
                     $.event.trigger({
                          type: "rebuildStoryboard",
                          message: "control have been removed!",
