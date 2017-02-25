31c2d1ebb4a6c6a7c47d732d13fe3fd9feb9d986
Tarek Wilkening <tarek_wilkening@web.de>
2017-02-25 12:08:59 +0100

    add new applet for ck-junctions

---
+++ js/ck-junctions/algorithm.js
@@ -0,0 +1,211 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// @author Tarek Wilkening (tarek_wilkening@web.de)
+//
+//////////////////////////////////////////////////////////////////////////////
+define(['jquery', 'three', '../lib/happah', '../lib/spherical-impostor', '../lib/util'], function($, THREE, HAPPAH, IMPOSTOR, UTIL) {
+     var s_controlPoints = Symbol('controlPoints');
+     var s_scrollbar = Symbol('scrollbar');
+     var s_camera = Symbol('camera');
+     var s_colors = Symbol('colors');
+     var s_color = Symbol('color');
+     var s_handles = Symbol('handles');
+
+     const left_segment_color = 0x54334f;
+     const middle_segment_color = 0x00dd00;
+     const right_segment_color = 0x0000dd;
+
+     class Algorithm {
+
+          /** Default constructor. */
+          constructor(controlPoints, scrollbar, camera) {
+               this.storyboard = this.storyboard.bind(this);
+               this[s_controlPoints] = controlPoints;
+               this[s_scrollbar] = scrollbar;
+               this[s_camera] = camera;
+               this[s_handles] = [];
+          }
+
+          /**
+           * Set scrollbar
+           */
+          set scrollbar(scrollbar) {
+               this[s_scrollbar] = scrollbar;
+               this[s_handles] = scrollbar.handles;
+
+               // We need an extra handle (TODO: this does not belong here)
+               //this[s_handles].push(this[s_scrollbar].addHandle(0.8, 0x33dd55));
+          }
+
+          /**
+           * Returns the intermediate point (vector) between two points a, b
+           * with distance from a set to |b-a|*ratio
+           */
+          interPointByRatio(a, b, ratio) {
+               // The direction is b - a
+               var direction = new THREE.Vector3().subVectors(b, a);
+
+               // Start point were we want to add the direction
+               var startPoint = a.clone();
+
+               // Add the scaled direction (scale factor defined by ratio)
+               return startPoint.add(direction.multiplyScalar(ratio));
+          }
+
+          subdivide(points, ratio) {
+               var newPoints = [];
+               // Iterate over points
+               for (var i = 0; i < points.length - 1; i++) {
+                    // Get the new point between point i and i+1
+                    newPoints.push(this.interPointByRatio(points[i], points[i + 1], ratio));
+               }
+               return newPoints;
+          }
+
+          evaluate(scrollbar) {
+               // TODO remove control points from pointMatrix.
+               var pointMatrix = [this[s_controlPoints].slice()];
+
+               // Get the first iteration
+               var newPoints = this.subdivide(this[s_controlPoints], this[s_scrollbar].valueOf(scrollbar));
+
+               pointMatrix.push(newPoints);
+
+               // Iterate over the new points and repeat
+               //var interPoints = this.subdivide(this[s_controlPoints], this[s_scrollbar].valueOf(1));
+
+               //pointMatrix.push(interPoints);
+
+
+               return pointMatrix;
+          }
+
+          /**
+           * Returns a storyboard with frames that contain the different steps
+           * of the algorithm
+           */
+          storyboard() {
+               // Create the first frame by hand
+               var storyboard = new HAPPAH.Storyboard(this);
+               var frame0 = new HAPPAH.Storyboard.Frame();
+               frame0.points = new THREE.Object3D();
+               frame0.title = "Controlpolygon";
+               storyboard.append(frame0);
+
+               if (this[s_controlPoints].length < 3) {
+                    // Add a dummy mesh
+                    frame0.lines[0] = new THREE.Object3D();
+                    storyboard.index = 0;
+                    return storyboard;
+               }
+               frame0.lines[0] = UTIL.Util.insertSegmentStrip(this[s_controlPoints], 0xff0000);
+               //         O
+               //        , ,            |
+               //       ,   + <-        v
+               //      ,     ,       O--+-------1
+               //  -> +       ,
+               //    ,         ,
+               //   O           O
+               var pointMatrixLeft = this.evaluate(0);
+               //         O
+               //        , ,
+               //    -> +   ,                |
+               //      ,     ,               v
+               //     ,       + <-   O-------+--1
+               //    ,         ,
+               //   O           O
+               var pointMatrixRight = this.evaluate(1);
+
+               // Helper points settings here
+               var color = 0x54334f;
+               var radius = 3;
+               var template = new IMPOSTOR.SphericalImpostor(radius);
+               template.material.uniforms.diffuse.value.set(color);
+
+
+               // 0        1
+               // O########O--------O--------O
+               var segment1 = [pointMatrixLeft[0][0], pointMatrixLeft[1][0]];
+               //          0        1
+               // O--------O########O--------O
+               var segment2 = [pointMatrixLeft[1][0], pointMatrixRight[1][0]];
+               //                   0        1
+               // O--------O--------O########O
+               var segment3 = [pointMatrixRight[1][0], pointMatrixLeft[0][1]];
+               // 0        1
+               // O########O--------O--------O
+               var segment4 = [pointMatrixLeft[0][1], pointMatrixLeft[1][1]];
+               //          0        1
+               // O--------O########O--------O
+               var segment5 = [pointMatrixLeft[1][1], pointMatrixRight[1][1]];
+               //                   0        1
+               // O--------O--------O########O
+               var segment6 = [pointMatrixRight[1][1], pointMatrixLeft[0][2]];
+
+               var frame1 = new HAPPAH.Storyboard.Frame();
+               frame1.lines.push(UTIL.Util.insertSegmentStrip(segment1, left_segment_color));
+               frame1.lines.push(UTIL.Util.insertSegmentStrip(segment2, middle_segment_color));
+               frame1.lines.push(UTIL.Util.insertSegmentStrip(segment3, right_segment_color));
+
+               frame1.lines.push(UTIL.Util.insertSegmentStrip(segment4, left_segment_color));
+               frame1.lines.push(UTIL.Util.insertSegmentStrip(segment5, middle_segment_color));
+               frame1.lines.push(UTIL.Util.insertSegmentStrip(segment6, right_segment_color));
+
+               var points = segment2.concat(segment5);
+               for (var i = 0; i < points.length; i++) {
+                    var imp = template.clone();
+                    imp.position.copy(points[i]);
+                    frame1.points.add(imp);
+               }
+               storyboard.append(frame1);
+
+               // Second iteration segments
+               var leftInterpoint1 = this.interPointByRatio(pointMatrixLeft[1][0], pointMatrixLeft[1][1], this[s_scrollbar].valueOf(0));
+               var leftInterpoint2 = this.interPointByRatio(pointMatrixLeft[1][0], pointMatrixLeft[1][1], this[s_scrollbar].valueOf(1));
+
+               var rightInterpoint1 = this.interPointByRatio(pointMatrixRight[1][0], pointMatrixRight[1][1], this[s_scrollbar].valueOf(0));
+               var rightInterpoint2 = this.interPointByRatio(pointMatrixRight[1][0], pointMatrixRight[1][1], this[s_scrollbar].valueOf(1));
+
+               // 0        1
+               // O########O--------O--------O
+               var segment1 = [pointMatrixLeft[1][0], leftInterpoint1];
+               //          0        1
+               // O--------O########O--------O
+               var segment2 = [leftInterpoint1, leftInterpoint2];
+               //                   0        1
+               // O--------O--------O########O
+               var segment3 = [leftInterpoint2, pointMatrixLeft[1][1]];
+               // 0        1
+               // O########O--------O--------O
+               var segment4 = [pointMatrixRight[1][0], rightInterpoint1];
+               //          0        1
+               // O--------O########O--------O
+               var segment5 = [rightInterpoint1, rightInterpoint2];
+               //                   0        1
+               // O--------O--------O########O
+               var segment6 = [rightInterpoint2, pointMatrixRight[1][1]];
+               //          v
+               // O--------O--------O--------O
+               var frame2 = frame1.clone();
+               var imp = template.clone();
+               imp.position.copy(this.interPointByRatio(pointMatrixLeft[1][0], pointMatrixLeft[1][1], this[s_scrollbar].valueOf(1)));
+               frame2.points.add(imp);
+
+               frame2.lines.push(UTIL.Util.insertSegmentStrip(segment1, left_segment_color));
+               frame2.lines.push(UTIL.Util.insertSegmentStrip(segment2, middle_segment_color));
+               frame2.lines.push(UTIL.Util.insertSegmentStrip(segment3, right_segment_color));
+
+               frame2.lines.push(UTIL.Util.insertSegmentStrip(segment4, left_segment_color));
+               frame2.lines.push(UTIL.Util.insertSegmentStrip(segment5, middle_segment_color));
+               frame2.lines.push(UTIL.Util.insertSegmentStrip(segment6, right_segment_color));
+
+               storyboard.append(frame2);
+
+               return storyboard;
+          }
+     }
+
+     return {
+          Algorithm: Algorithm
+     };
+});
---
+++ js/ck-junctions/main.js
@@ -0,0 +1,74 @@
+require.config({
+     baseUrl: '../../js',
+     shim: {
+          'bootstrap': {
+               deps: ['jquery']
+          },
+          'three': {
+               exports: 'THREE'
+          },
+          'TrackballControls': {
+               deps: ['three'],
+               exports: 'THREE'
+          },
+          'TransformControls': {
+               deps: ['three'],
+               exports: 'THREE'
+          }
+     },
+     paths: {
+          i18n: "http://rawgit.com/fnando/i18n-js/master/app/assets/javascripts/i18n",
+          impromptu: "https://cdn.rawgit.com/trentrichardson/jQuery-Impromptu/master/dist/jquery-impromptu.min",
+          mathjax: "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML",
+          jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min",
+          three: "https://cdnjs.cloudflare.com/ajax/libs/three.js/r83/three",
+          TrackballControls: "http://threejs.org/examples/js/controls/TrackballControls",
+          TransformControls: "http://threejs.org/examples/js/controls/TransformControls",
+          bootstrap: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap",
+     }
+});
+
+require(['../lib/happah', '../lib/defaults', './algorithm', '../lib/twohandlescrollbar', '../lib/pointcontrols', 'three', 'jquery'], function(happah, DEFAULTS, ALGORITHM, SCROLLBAR, CONTROLS, THREE, $) {
+     // Canvas element
+     var canvas = $('.hph-canvas')[0];
+     var scene = new happah.Scene();
+
+     // Points & impostors
+     var points = [];
+     var impostors = new THREE.Object3D();
+
+     scene.add(impostors);
+
+     // Canvas coordinates relative to middle of canvas element
+     var pos = new THREE.Vector3(0, -(1 / 1.2), 0);
+     var algorithm = new ALGORITHM.Algorithm(points);
+
+     var viewport = new happah.Viewport(canvas, scene, algorithm);
+
+     var scrollbar = new SCROLLBAR.TwoHandleScrollbar(pos, viewport, 0.2);
+
+     var dragControls = new happah.DragControls(impostors.children, viewport.controls, viewport.camera);
+     dragControls.listenTo(viewport.renderer.domElement);
+
+     algorithm.scrollbar = scrollbar;
+     scrollbar.listenTo(viewport.renderer.domElement);
+     viewport.overlay.add(scrollbar);
+     viewport.camera.position.set(1000, 1000, 0);
+     viewport.camera.lookAt(scene.position);
+     viewport.camera.zoom = 2.5;
+     viewport.camera.updateProjectionMatrix();
+
+     var pointControls = new CONTROLS.PointControls(impostors, points, viewport.camera, 0);
+     pointControls.listenTo(viewport.renderer.domElement);
+
+     // Initialize some points
+     pointControls.addControlPoints([
+          new THREE.Vector3(50, 0, -60), new THREE.Vector3(-50, 0, 0),
+          new THREE.Vector3(50, 0, 60)
+     ]);
+
+     //var menu = new happah.Menu(".btn-group", algorithm);
+     var toolbar = DEFAULTS.Defaults.toolbarMenu(".tool-bar-top");
+     var menu = DEFAULTS.Defaults.playerMenu("#hph-controls");
+     console.log("happah initialized.");
+});
