1f6d0a64ba6019f521631abf5f29d4c363eeaf09
Tarek Wilkening <tarek_wilkening@web.de>
2017-04-20 15:55:52 +0200

    make label store parent object3D instead of vector

--- js/lib/labelmanager-linked.js
+++ js/lib/labelmanager-linked.js
@@ -33,17 +33,19 @@
             * Add a new label at the given position.
             * If overview = true, the label will be projected by the overlay
             * camera
+            * -> will follow any parent object3D.
             */
-           addLabel(text, position, tag = "", overlay = false) {
+           addLabel(text, parent, tag = "", overlay = false) {
                 var label = null;
                 if (!this[s_head]) {
-                     this[s_head] = new Label(text, position, tag, overlay, null, this[s_viewport]);
+                     this[s_head] = new Label(text, parent, tag, overlay, null, this[s_viewport]);
                      this[s_tail] = this[s_head];
                 } else {
-                     label = new Label(text, position, tag, overlay, null, this[s_viewport]);
+                     label = new Label(text, parent, tag, overlay, null, this[s_viewport]);
                      this[s_tail].next = label
                      this[s_tail] = label;
                 }
+                return label;
            }
 
            /**
@@ -105,18 +107,15 @@
             * that was given when added
             */
            updatePositions() {
-                console.log("update positions triggered!");
                 var iterator = this[s_head];
                 if (!iterator) {
-                     return;
-                } else if (!iterator.next) {
-                     iterator.updatePosition();
                      return;
                 }
                 while (iterator.next) {
                      iterator.updatePosition();
                      iterator = iterator.next;
                 }
+                iterator.updatePosition();
            }
 
       } // Class LabelManager
@@ -124,14 +123,16 @@
       var s_next = Symbol('next');
       var s_htmlObject = Symbol('htmlobject');
       var s_tag = Symbol('tag');
-      var s_position = Symbol('position');
+      var s_parent = Symbol('s_parent');
       var s_sceneCamera = Symbol('scenecamera');
       var s_overlayCamera = Symbol('overlaycamera');
+      var s_text = Symbol('text');
 
       class Label {
 
-           constructor(text, position, tag = "", overlay = false, next, viewport) {
-                this[s_position] = position;
+           constructor(text, parent, tag = "", overlay = false, next, viewport) {
+                this[s_text] = text;
+                this[s_parent] = parent;
                 this[s_next] = next;
                 this[s_tag] = tag;
                 this[s_overlayCamera] = viewport.overlayCam;
@@ -151,37 +152,24 @@
                 this[s_htmlObject].css("color", label_color);
                 this[s_htmlObject].css("font-size", label_font_size);
 
-                // Transform world-position vector to pixel coordinates
-                var pos = position.clone();
+                // Make it non-selectable
+                this[s_htmlObject].css("-webkit-touch-callout", "none");
+                this[s_htmlObject].css("-webkit-user-select", "none");
+                this[s_htmlObject].css("-khtml-user-select", "none");
+                this[s_htmlObject].css("-moz-user-select", "none");
+                this[s_htmlObject].css("-ms-user-select", "none");
+                this[s_htmlObject].css("user-select", "none");
 
                 if (overlay) {
-                     pos.project(this[s_overlayCamera]);
                      this[s_htmlObject].addClass('overlay');
-                } else {
-                     pos.project(this[s_sceneCamera]);
                 }
-                //pos.project(this[s_overlayCamera]);
-
-                pos.x = Math.round((pos.x + 1) * this.canvas.width / 2);
-                pos.y = Math.round((-pos.y + 1) * this.canvas.height / 2);
-
-                // Limit to canvas frame
-                pos.x = (pos.x > this.canvas.width - 20) ? this.canvas.width - 20 : pos.x;
-                pos.y = (pos.y > this.canvas.height - 20) ? this.canvas.height - 20 : pos.y;
-
-                pos.max(new THREE.Vector3(0, 0, 0));
-
-                this[s_htmlObject].css("left", pos.x + "px");
-                this[s_htmlObject].css("top", pos.y + "px");
-
-                // Add text to the label
+                this.updatePosition();
                 this[s_htmlObject].append(text);
            }
+
            updatePosition() {
-                //if (this[s_htmlObject].hasClass("overlay")) {
-                //return;
-                //}
-                var pos = this[s_position].clone();
+                var pos = this[s_parent].isVector3 ? this[s_parent].clone() : this[s_parent].getWorldPosition();
+                //var pos = this[s_parent].position.clone();
 
                 if (this[s_htmlObject].hasClass("overlay")) {
                      pos.project(this[s_overlayCamera]);
@@ -213,6 +201,9 @@
            }
            get tag() {
                 return this[s_tag];
+           }
+           get text() {
+                return this[s_text];
            }
 
       } // class Label
